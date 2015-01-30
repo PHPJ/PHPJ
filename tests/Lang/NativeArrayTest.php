942099a6b31064d637befc54d164fdcfbb131744
Yury Kozyrev <ykmship@yandex-team.ru>
2015-01-30 15:50:48 +0300

    NativeArray test

---
+++ tests/Lang/NativeArrayTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Yury Kozyrev [https://github.com/urakozz]
+ */
+
+namespace PHPJ\Tests\Lang;
+
+
+use PHPJ\Tests\Test;
+
+class NativeArrayTest  extends Test
+{
+
+  protected function getClassName()
+  {
+    return '\PHPJ\Lang\NativeArray';
+  }
+}
