932917309cae3d28e7bf8179c1c6066be9093b91
Yury Kozyrev <ykmship@yandex-team.ru>
2015-02-02 18:41:58 +0300

    test update

--- tests/Test.php
+++ tests/Test.php
@@ -13,9 +13,11 @@ abstract class Test extends PHPUnit_Framework_TestCase
     $name = $this->getClassName();
     $this->assertTrue(class_exists($name));
     $r = new \ReflectionClass($name);
-    $class = $r->newInstanceWithoutConstructor();
-    $this->assertInstanceOf($name, $class);
-    $this->assertInstanceOf('PHPJ\Lang\Object', $class);
+    if(PHP_VERSION_ID >= 50600 || !$r->isInternal()){
+      $class = $r->newInstanceWithoutConstructor();
+      $this->assertInstanceOf($name, $class);
+      $this->assertInstanceOf('PHPJ\Lang\Object', $class);
+    }
   }
 
   abstract protected function getClassName();
