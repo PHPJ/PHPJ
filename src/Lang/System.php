edb16df21e7021801dc3ca07a88a911a8700c6c1
Yury Kozyrev <ykmship@yandex-team.ru>
2015-02-02 18:15:29 +0300

    system update

--- src/Lang/System.php
+++ src/Lang/System.php
@@ -12,27 +12,31 @@ final class System extends ObjectClass
 
   public static function arraycopy(NativeArray $src, $srcPos, NativeArray $dest, $destPos, $length)
   {
-    $srcPos = (int)$srcPos;
-    $destPos = (int)$destPos;
-    $length = (int)$length;
-    if ($srcPos < 0) {
-      throw new ArrayIndexOutOfBoundsException($srcPos);
+    self::checkArrayCopy($src, $srcPos, $dest, $destPos, $length);
+    for ($i = 0; $i < $length; $i++) {
+      $dest[$destPos + $i] = $src[$srcPos + $i];
     }
-    if ($destPos < 0) {
-      throw new ArrayIndexOutOfBoundsException($destPos);
-    }
-    if ($length < 0) {
-      throw new ArrayIndexOutOfBoundsException();
-    }
+    return $dest;
+  }
+
+  protected static function checkArrayCopy(NativeArray $src, &$srcPos, NativeArray $dest, &$destPos, &$length)
+  {
+    self::checkIsPositive($srcPos);
+    self::checkIsPositive($destPos);
+    self::checkIsPositive($length);
     if ($srcPos + $length > $src->length()) {
       throw new ArrayIndexOutOfBoundsException();
     }
     if ($destPos + $length > $dest->length()) {
       throw new ArrayIndexOutOfBoundsException();
     }
-    for ($i = 0; $i < $length; $i++) {
-      $dest[$destPos + $i] = $src[$srcPos + $i];
+  }
+
+  protected static function checkIsPositive(&$index)
+  {
+    $index = (int)$index;
+    if ($index < 0) {
+      throw new ArrayIndexOutOfBoundsException($index);
     }
-    return $dest;
   }
 }
